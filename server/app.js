ee9bd3f1091124c8ddc8a76891209abf001a8410
Tsapu <chooky823@gmail.com>
2022-10-28 17:55:49 +0300

    cleaning

--- server/app.js
+++ server/app.js
@@ -7,19 +7,11 @@ var cors = require('cors');
 
 require('dotenv').config()
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
-// console.log(`Running ${process.env.NODE_ENV} environment`);
+console.log(`Running ${process.env.NODE_ENV} server`);
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 var app = express();
 var expressWs = require('express-ws')(app);
 var containerAPI = require('./routes/api'); // must be loaded after setting up ws
-
-// const { proxy, scriptUrl } = require('rtsp-relay')(app);
-// const handler = proxy({
-//   url: `rtsp://admin:@192.168.200.211:554/h264Preview_01_sub`,
-//   verbose: false,
-// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,34 +23,9 @@ app.use(express.json()); // body-parser
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
+// ------------------------------
 app.use('/', containerAPI)
-//app.ws('/api/stream', handler);
-
-var db = require('./data/db.js');
-
-// app.ws('/api/stream/:workcell', (ws, req) => {
-//   const { workcell } = req.params  
-//   const query_cell = parseInt(workcell)
-//   if (!query_cell) {
-//     console.log("Invalid stream param")
-//     return ws.close()
-//   }
-//   db('cameras').first()
-//     .where({ cell: query_cell })
-//     .then( ({ camera_ip }) => {
-//       // console.log(camera_ip);
-//       proxy({
-//         url: `rtsp://${process.env.CAM_CREDENTIALS}@${camera_ip}:554/h264Preview_01_main`,
-//         verbose: false,
-//       })(ws)
-//     })
-// });
-
-// serve frontend at index
-// app.use(express.static(path.join(__dirname, 'client/build')));
+// ------------------------------
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/util/time.js
+++ server/util/time.js
@@ -2,12 +2,12 @@
 
 const verifyTimeInterval = (bookings) => {
 	let currentDate = new Date();
-	//const dateISO = currentDate.toISOString();
+
 	const timeSlot = bookings.find(booking => {
 		const { start, end } = booking;
 		const startDate = new Date(start);
 		const endDate = new Date(end);
-		// console.log(startDate < currentDate, endDate > currentDate)
+
 		// Check if the current time falls within the allowed interval
 		// if the current time is between the start and end time, then user has access to their booking
 		if (startDate < currentDate && endDate > currentDate) {
